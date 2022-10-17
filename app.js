f67de63dfd377744ded010862b37e3c63884d10e
saimsiddiqui8 <saim.ali.siddiqui2004@gmail.com>
2022-10-17 23:48:27 +0500

    2ndDay
    Some upgrade in app

--- app.js
+++ app.js
@@ -89,7 +89,6 @@ const key = [
     },
 ];
 
-
 const questions = document.querySelector(".question");
 const ans1 = document.querySelector("#option1");
 const ans2 = document.querySelector("#option2");
@@ -100,88 +99,13 @@ const submit = document.querySelector(".submit");
 const result = document.querySelector("#result");
 const percentage = document.querySelector(".percentage");
 const passFailText = document.querySelector("#pass_fail_text");
-// console.log(circle[1]);
 const btnz = document.querySelector("#btnzs");
+const circle = document.getElementById('crc');
+const paraDisplay = document.getElementById('paraDisplay');
 let arrayCount = 0;
 let scores = 0;
-const circle = document.getElementById('crc')
-// let attributeVal = circle.getAttribute("stroke-dasharray");
-
-// console.log(attributeVal);
 
 
-
-
-
-
-
-function hide() {
-    start();
-    arrayCount = 1;
-    document.querySelector("#start").style.display = "none";
-    document.querySelector("#round").style.display = "none";
-    // document.querySelector("#choose").style.display = "block";
-    document.querySelector("#rules").style.display = "block";
-    document.body.style.display = "block";
-    document.body.style.height = "40vh";
-    document.querySelector(".breakTag").style.display = "none";
-}
-
-btnz.addEventListener("click", function () {
-    document.querySelector("#quizStart").style.display = "block";
-    document.querySelector("#ul1").style.display = "block";
-    document.querySelector("#ul2").style.display = "block";
-    document.querySelector("#ul3").style.display = "block";
-    document.querySelector("#ul4").style.display = "block";
-    document.querySelector("#rules").style.display = "none";
-    
-});
-
-let btn = document.getElementById("btns");
-btn.addEventListener("click", function () {
-    document.body.style.background = "#1D1F20"
-});
-
-submit.addEventListener("click", function () {
-    start();
-    submit.innerHTML = "Next"
-
-
-    
-    if (arrayCount == 9) {
-        submit.innerHTML = "Submit"
-
-    }
-    if (arrayCount == 10) {
-        result.style.display = "block";
-        percentage.innerHTML = `${scores}0%`
-        quizStart.style.display = "none";
-        let t = circle.setAttribute("stroke-dasharray", scores + "0" + ", 100");
-    }
-    if (scores == 0) {
-        percentage.innerHTML = "0%"
-    }  
-    if (scores < 6) {
-        passFailText.innerHTML = 'You Failed !'
-        
-        
-    } else {
-        passFailText.innerHTML = "Congratz You passed !"
-        
-        
-        
-    }
-    let check = getcheckAnswer();
-    console.log(check);
-    console.log("Orignal " + key[arrayCount - 1].ans);
-
-    if (arrayCount > 0) {
-        if (check === key[arrayCount - 1].ans) {
-            scores++
-        }
-    }
-    arrayCount++;
-});
 
 function start() {
     let structure = key[arrayCount]
@@ -201,3 +125,105 @@ function getcheckAnswer() {
     });
     return answer;
 };
+
+function winSound() {
+    var audio = new Audio('./success.mp3');
+    audio.loop = false;
+    audio.play();
+}
+
+function loseSound() {
+    var audio = new Audio('./fail.mp3');
+    audio.loop = false;
+    audio.play();
+}
+
+
+function hide() {
+    start();
+    arrayCount = 1;
+    document.querySelector("#start").style.display = "none";
+    document.querySelector("#round").style.display = "none";
+    document.querySelector("#rules").style.display = "block";
+    document.body.style.display = "block";
+    document.body.style.height = "40vh";
+    document.querySelector(".breakTag").style.display = "none";
+}
+
+btnz.addEventListener("click", function () {
+    document.querySelector("#quizStart").style.display = "block";
+    document.querySelector("#ul1").style.display = "block";
+    document.querySelector("#ul2").style.display = "block";
+    document.querySelector("#ul3").style.display = "block";
+    document.querySelector("#ul4").style.display = "block";
+    document.querySelector("#rules").style.display = "none";
+
+});
+
+let btn = document.getElementById("btns");
+btn.addEventListener("click", function () {
+    document.body.style.background = "#1D1F20"
+});
+
+submit.addEventListener("click", function () {
+    start();
+    submit.innerHTML = "Next"
+
+
+
+    if (arrayCount == 9) {
+        submit.innerHTML = "Submit"
+
+    }
+    if (arrayCount == 10) {
+        result.style.display = "block";
+        quizStart.style.display = "none";
+        percentage.innerHTML = `${scores}0%`
+        paraDisplay.innerHTML = scores;
+        let t = circle.setAttribute("stroke-dasharray", scores + "0" + ", 100");
+        if (scores < 6) {
+            loseSound();
+        }
+        if (scores >= 6) {
+            winSound();
+        }
+    }
+    if (scores == 0) {
+        percentage.innerHTML = "0%"
+    }
+    if (scores < 6) {
+        passFailText.innerHTML = 'You Failed !'
+        circle.style.stroke = 'red';
+        percentage.style.fill = 'red';
+        paraDisplay.style.color = 'red';
+    }
+    if (scores >= 6) {
+        passFailText.innerHTML = "Congratz You passed !"
+        circle.style.stroke = '#07d000';
+        percentage.style.fill = '#07d000';
+        paraDisplay.style.color = '#07d000';
+    }
+    if (scores > 8) {
+        passFailText.innerHTML = 'You pass with flying colors !'
+        circle.style.stroke = '#daa520';
+        percentage.style.fill = '#daa520';
+        paraDisplay.style.color = '#daa520';
+    }
+
+
+
+
+    let check = getcheckAnswer();
+    console.log(check);
+    console.log("Orignal " + key[arrayCount - 1].ans);
+
+    if (arrayCount > 0) {
+        if (check === key[arrayCount - 1].ans) {
+            scores++;
+        }
+    }
+    arrayCount++;
+});
+
+
+
